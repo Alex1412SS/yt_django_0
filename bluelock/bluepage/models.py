798b342cbcd0e30aa553cc05a6ef5b1134105ce0
Алексей <blameofforg@gmail.com>
2024-03-24 22:43:32 +0300

    shablon and images field

--- bluelock/bluepage/models.py
+++ bluelock/bluepage/models.py
@@ -5,6 +5,7 @@ class Ai_tools(models.Model):
     about = models.CharField(max_length=400)
     data_time = models.DateTimeField()
     link_to = models.CharField(max_length=300)
+    image = models.ImageField(blank=True, upload_to='Images')
 
     def __str__(self):
         return self.name
---
+++ bluelock/bluepage/static/bluepage/js/jssite.js
@@ -0,0 +1,14 @@
+$(function(){
+    $('body').fadeOut(0)
+    $('body').fadeIn(1000)
+    $('.set_menu').hide()
+$(".open_set").click(function(){
+
+    $('.set_menu').show(200)
+})
+$(".set_menu").mouseleave(function(){
+    $('.set_menu').slideUp(200)
+    $(".open_set").addClass('open_set text-[#ECECEC] px-6 p-2 rounded-full duration-200 hover:text-black hover:bg-green-500 mr-12')
+})
+});
+
