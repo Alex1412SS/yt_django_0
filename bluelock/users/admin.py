eae419f348547725340114b878a20490a446cea0
Alex1412SS <155660176+Alex1412SS@users.noreply.github.com>
2024-06-02 15:11:08 +0300

    Delete bluelock directory

--- bluelock/users/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import profile_model
-
-admin.site.register(profile_model)
--- bluelock/users/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import User
-class profile_model(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
-    image = models.ImageField(blank=True, upload_to='profile_images')
-
